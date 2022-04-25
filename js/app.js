e2340c08d24b3a2220d48c31773fb8ce4f43cb1f
arbp97 <arbp97@outlook.com>
2022-04-24 22:26:26 -0300

    add fontawesome, change nav button links to listeners

--- js/app.js
+++ js/app.js
@@ -1,7 +1,25 @@
+import { setActive, removeActive } from "../js/util.js";
+
+// routing function
 function goTo(sectionId) {
-  Array.from(document.querySelectorAll("main>section")).forEach(
-    (section) => (section.style.display = "none")
-  );
-  document.querySelector("#" + sectionId).style.display = "flex";
+  Array.from(document.querySelectorAll("main>section")).forEach((section) => {
+    section.style.display = "none";
+    removeActive(section.id + "-button");
+  });
+  document.getElementById(sectionId).style.display = "flex";
+  setActive(sectionId + "-button");
 }
+
+// default route
 goTo("about");
+
+// load listeners to buttons
+Array.from(document.querySelectorAll("main>section")).forEach((section) => {
+  document.getElementById(section.id + "-button").addEventListener(
+    "click",
+    function () {
+      goTo(section.id);
+    },
+    false
+  );
+});
---
+++ js/util.js
@@ -0,0 +1,7 @@
+export function setActive(id) {
+  document.getElementById(id).classList.add("active");
+}
+
+export function removeActive(id) {
+  document.getElementById(id).classList.remove("active");
+}
