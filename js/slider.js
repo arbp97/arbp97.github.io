610f56106e9b3b64dab9e6b78be0710abafab4c9
arbp97 <arbp97@outlook.com>
2022-04-28 19:23:06 -0300

    load projects from file

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,42 @@
-document.addEventListener("DOMContentLoaded", function () {
+// fetch project data from json file, append every
+// instance in project slider
+async function loadProjects() {
+  const data = await fetch("../json/projects.json");
+  const list = await data.json();
+
+  list.forEach((project) => {
+    // create elements
+    let splideSlide = document.createElement("li");
+    let splideContainer = document.createElement("div");
+    let image = document.createElement("img");
+    let title = document.createElement("p");
+    let description = document.createElement("div");
+
+    // set classes and attributes
+    splideSlide.classList.add("splide__slide");
+    splideContainer.classList.add("splide__slide__container");
+    image.setAttribute("src", "img/" + project.file);
+    image.setAttribute("alt", "project-img");
+    title.classList.add("project-title");
+    title.textContent = project.name;
+    description.classList.add("project-description");
+    description.textContent = project.description;
+
+    // mount everything
+    splideContainer.appendChild(image);
+    splideSlide.appendChild(splideContainer);
+    splideSlide.appendChild(title);
+    splideSlide.appendChild(description);
+
+    // append to slider
+    document.getElementById("project-slider-list").appendChild(splideSlide);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+  //load projects before initializing splide
+  await loadProjects();
+
   new Splide("#project-slider", {
     perPage: 2,
     gap: "1rem",
