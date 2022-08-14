384af9af0e82309acbe9b7dee5a5ffa236fc5132
arbp97 <alanblangille@gmail.com>
2022-08-14 12:48:28 -0300

    add react spring

--- src/sections/Landing.tsx
+++ src/sections/Landing.tsx
@@ -1,5 +1,12 @@
+import { useSpring, animated } from "react-spring";
+
 const Landing = () => {
-  return <section id="landing">LANDING</section>;
+  const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } });
+  return (
+    <section id="landing">
+      <animated.div style={props}>I will fade in</animated.div>
+    </section>
+  );
 };
 
 export default Landing;
