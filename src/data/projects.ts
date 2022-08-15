7a488bbb11173e1d8c4a434ea464b084682e02c5
arbp97 <alanblangille@gmail.com>
2022-08-15 15:34:41 -0300

    add project data and images

---
+++ src/data/projects.ts
@@ -0,0 +1,25 @@
+export const PROJECTS = [
+  {
+    name: "Saint Patrick Homebanking",
+    description:
+      "Web App that simulates an E-Wallet, focused on money transactions in multiple currencies and account information. Frontend: React, Styled Components. Backend: NodeJS, Express, JWT, MongoDB. Demo Credentials: anarod@gmail.com BushidoTrooper123",
+    file: "projects/saint-patrick.png",
+    link: "https://github.com/arbp97/hb-api/",
+    live: "https://home-banking-psi.vercel.app/",
+  },
+  {
+    name: "McDonald's App Clone",
+    description:
+      "Progressive Web App emulating the style and functionality the native original app of McDonald's. This project was developed for an assignment at UNLa. Stack: Typescript, React, Bootstrap, CSS.",
+    file: "projects/mcapp.png",
+    link: "https://github.com/arbp97/mcapp/",
+    live: "https://mcdapp.vercel.app/",
+  },
+  {
+    name: "BTree View",
+    description:
+      "Desktop App based on C, GTK 3 and Glade. It helps you build and visualize binary trees, make use of various arrangement methods. Also includes AVL tree auto-balancing.",
+    file: "projects/btree-view.png",
+    link: "https://github.com/arbp97/btree-view",
+  },
+];
