2db9b5db89d4c9e3b42e1b63aeae1c54c686960a
arbp97 <alanblangille@gmail.com>
2022-08-15 16:56:46 -0300

    add content to projects section

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,7 +3,12 @@ export const PROJECTS = [
     name: "Saint Patrick Homebanking",
     description:
       "Web App that simulates an E-Wallet, focused on money transactions in multiple currencies and account information. Frontend: React, Styled Components. Backend: NodeJS, Express, JWT, MongoDB. Demo Credentials: anarod@gmail.com BushidoTrooper123",
-    file: "projects/saint-patrick.png",
+    files: [
+      "projects/st-patrick-1.png",
+      "projects/st-patrick-2.png",
+      "projects/st-patrick-3.png",
+      "projects/st-patrick-4.png",
+    ],
     link: "https://github.com/arbp97/hb-api/",
     live: "https://home-banking-psi.vercel.app/",
   },
@@ -11,7 +16,13 @@ export const PROJECTS = [
     name: "McDonald's App Clone",
     description:
       "Progressive Web App emulating the style and functionality the native original app of McDonald's. This project was developed for an assignment at UNLa. Stack: Typescript, React, Bootstrap, CSS.",
-    file: "projects/mcapp.png",
+    files: [
+      "projects/mcapp-1.png",
+      "projects/mcapp-2.png",
+      "projects/mcapp-3.png",
+      "projects/mcapp-4.png",
+      "projects/mcapp-5.png",
+    ],
     link: "https://github.com/arbp97/mcapp/",
     live: "https://mcdapp.vercel.app/",
   },
@@ -19,7 +30,7 @@ export const PROJECTS = [
     name: "BTree View",
     description:
       "Desktop App based on C, GTK 3 and Glade. It helps you build and visualize binary trees, make use of various arrangement methods. Also includes AVL tree auto-balancing.",
-    file: "projects/btree-view.png",
+    files: ["projects/btree-view.png"],
     link: "https://github.com/arbp97/btree-view",
   },
 ];
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,13 +1,53 @@
 import { useThemeContext } from "../context/ThemeContext";
+import { PROJECTS } from "../data/projects";
+import { IMG_PATH } from "../config";
+import { Galleria } from "primereact/galleria";
+import { Image } from "primereact/image";
 
 const Projects = () => {
   const { currentTheme } = useThemeContext();
+
+  const itemTemplate = (item: any) => {
+    return <Image src={IMG_PATH + item.src} alt={""} width="100%" preview />;
+  };
+
   return (
     <section
       id="projects"
-      className={currentTheme ? "bg-bluegray-900" : "bg-primary-100"}
+      className={
+        "flex flex-column justify-content-center align-items-center gap-4 pb-8" +
+        (currentTheme ? " bg-bluegray-900" : " bg-primary-100")
+      }
     >
-      PROJECTS
+      <p className="text-5xl font-bold mt-0 mb-8 text-primary">PROJECTS</p>
+      {PROJECTS.map((value, index) => {
+        return (
+          <div
+            key={index}
+            className="flex flex-column lg:flex-row justify-content-center w-full h-auto gap-8"
+          >
+            <div className="w-full md:w-3 h-auto flex flex-column gap-1">
+              <p className="text-primary text-2xl font-bold">{value.name}</p>
+              <p className="text-color text-xl line-height-2">
+                {value.description}
+              </p>
+            </div>
+            <Galleria
+              className="w-full lg:w-6"
+              value={value.files.map((element) => ({
+                src: element,
+              }))}
+              item={itemTemplate}
+              style={{ maxWidth: "768px" }}
+              numVisible={5}
+              showThumbnails={false}
+              showIndicators
+              changeItemOnIndicatorHover
+              showIndicatorsOnItem
+            />
+          </div>
+        );
+      })}
     </section>
   );
 };
